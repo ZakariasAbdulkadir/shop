5c03adc6e4ef345cf322c4f53cd6354fd61a79f6
Zakarias Abdulkadir <z.k.abdulkadir@gmail.com>
2021-12-09 15:57:36 -0700

    Api routes created

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Liz',
+    email: 'liz@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+]
+
+export default users
