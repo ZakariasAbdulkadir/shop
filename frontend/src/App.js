ee5fd16eeae8153ce66a2fcdb0a283170d3d6dd4
Zakarias Abdulkadir <z.k.abdulkadir@gmail.com>
2021-12-15 09:42:51 -0700

    cart state and screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 function App() {
   return (
     <Router>
-      <Header/>
+      <Header />
       <main className='py-3'>
         <Container>
           <Routes>
             <Route path='/' element={<HomeScreen />} exact />
             <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/cart/:id' element={<CartScreen />} />
+            <Route path='/cart' element={<CartScreen />} />
           </Routes>
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,23 +1,29 @@
-import React, { useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { listProductDetails } from '../actions/productActions'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 
 const ProductScreen = () => {
-  const { id } = useParams()
+  const [qty, setQty] = useState(1)
 
+  const { id } = useParams()
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const productDetails = useSelector(state => state.productDetails)
   const { loading, error, product } = productDetails
 
   useEffect(() => {
     dispatch(listProductDetails(id))
-  }, [id])
+  }, [id, dispatch])
+
+  const addToCartHandler = () => {
+    navigate(`/cart/${id}?qty=${qty}`)
+  }
 
   return (
     <>
@@ -69,8 +75,33 @@ const ProductScreen = () => {
                     </Col>
                   </Row>
                 </ListGroup.Item>
+
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={qty}
+                          onChange={e =>
+                            (e.target.value = setQty(e.target.value))
+                          }
+                        >
+                          {[...Array(product.countInStock).keys()].map(x => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
                 <ListGroup.Item>
                   <Button
+                    onClick={addToCartHandler}
                     className='w-100'
                     type='button'
                     disabled={product.countInStock === 0}
