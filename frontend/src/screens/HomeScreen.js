ebe0bb84df1e936cb4955f419a71d596ebfcaa8c
Zakarias Abdulkadir <z.k.abdulkadir@gmail.com>
2022-01-04 01:34:40 -0700

    Prepare for deployment

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -32,15 +32,6 @@ const HomeScreen = () => {
           ))}
         </Row>
       )}
-      <iframe
-        width='560'
-        height='315'
-        src='https://www.youtube.com/embed/KL8mOb6H9N4'
-        title='YouTube video player'
-        frameborder='0'
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-        allowfullscreen
-      ></iframe>
     </>
   )
 }
